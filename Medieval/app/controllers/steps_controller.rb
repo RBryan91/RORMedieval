b322c1fd7711e63a9ec9f0ae29d708d2a6aa8428
Renaudin Bryan <bryan.renaudin@epitech.eu>
2023-12-08 16:30:40 +0100

    Add : Edit Step

--- Medieval/app/controllers/steps_controller.rb
+++ Medieval/app/controllers/steps_controller.rb
@@ -5,6 +5,24 @@ class StepsController < ApplicationController
         @step = Step.new
       end
 
+    def show
+        @step = Step.find(params[:id])
+      end
+
+    def edit
+        @step = Step.find(params[:id])
+      end  
+
+    def update
+      @step = Step.find(params[:id])
+  
+        if @step.update(step_params)
+          redirect_to @step
+        else
+          render :edit, status: :unprocessable_entity
+        end
+      end
+
     def create
         @step = Step.new(step_params)
         session[:check] = 1
