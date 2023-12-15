07b98b203cf56aca3c5daf971dc0d37ba2e69e7a
Renaudin Bryan <bryan.renaudin@epitech.eu>
2023-12-15 11:20:48 +0100

    Add : EnigmeVu

--- Medieval/app/controllers/enigmes_controller.rb
+++ Medieval/app/controllers/enigmes_controller.rb
@@ -15,14 +15,27 @@ class EnigmesController < ApplicationController
       @enigme_id = @enigme_params[:id]
       @enigme = Enigme.find(@enigme_id)
       @answers = Answer.find_by(enigme_id:@enigme.id)
-     
+      @quest = Quest.find(session[:current_quest])
+      @step = Step.find(session[:current_step])
+      @character = current_character
+      @enigme_message = []
      
       if params[:reponse] == @answers[@answers.true]
+        @enigme_message << "VICTORY !"
+        @enigme_message << "You answered with BRIO !"
+        @enigme_message << "You earn #{@step.xp} xp after this enigme !"
+        @enigme_message << "You finish the step #{@step.titre} !"
+
+        if level_up(@step.xp)
+          @enigme_message << "You passed level #{@character.level} !!! Go back to your character menu to split your characteristics points."
+        end
         @win = true
       else
+        @enigme_message << "GAME OVER !"
         @win = false
        end
-       redirect_to enigme_path(@enigme,win:@win)
+       session[:enigme_message ] = @enigme_message
+       redirect_to enigme_path(@enigme,win:@win,quest_id:@quest.id,step_id:@step.id)
      end
    
       def create
